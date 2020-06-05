8ed0398238c9c3830221f14c6470e215933a4cb8
long-michaelj1990 <65982679+long-michaelj1990@users.noreply.github.com>
2020-06-05 13:51:44 +0100

    Add files via upload

---
+++ EMG_Test.ino
@@ -0,0 +1,117 @@
+/* http://www.youtube.com/c/electronoobs
+ * 
+ * This is an example where we configure te data of the MPU6050
+ * and read the gyro data and print it to the serial monitor
+ * 
+ * Arduino pin    |   MPU6050
+ * 5V             |   Vcc
+ * GND            |   GND
+ * A4             |   SDA
+ * A5             |   SCL
+ */
+ 
+//Includes
+#include <Wire.h>
+
+
+//Variables
+float elapsedTime, time, timePrev;        //Variables for time control
+int gyro_error=0;                         //We use this variable to only calculate once the gyro data error
+int16_t Gyr_rawX, Gyr_rawY, Gyr_rawZ;     //Here we store the raw data read 
+float Gyro_angle_x, Gyro_angle_y;         //Here we store the angle value obtained with Gyro data
+float Gyro_raw_error_x, Gyro_raw_error_y; //Here we store the initial gyro data error
+
+
+
+void setup() {
+  Wire.begin();                           //begin the wire comunication  
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68)              
+  Wire.write(0x6B);                       //make the reset (place a 0 into the 6B register)
+  Wire.write(0x00);
+  Wire.endTransmission(true);             //end the transmission
+  
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68) 
+  Wire.write(0x1B);                       //We want to write to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10);                       //Set the register bits as 00010000 (1000dps full scale)
+  Wire.endTransmission(true);             //End the transmission with the gyro
+
+  Serial.begin(9600);                     //Remember to set this same baud rate to the serial monitor  
+  time = millis();                        //Start counting time in milliseconds
+
+
+
+/*Here we calculate the gyro data error before we start the loop
+ * I make the mean of 200 values, that should be enough*/
+  if(gyro_error==0)
+  {
+    for(int i=0; i<200; i++)
+    {
+      Wire.beginTransmission(0x68);            //begin, Send the slave adress (in this case 68) 
+      Wire.write(0x43);                        //First adress of the Gyro data
+      Wire.endTransmission(false);
+      Wire.requestFrom(0x68,4,true);           //We ask for just 4 registers 
+         
+      Gyr_rawX=Wire.read()<<8|Wire.read();     //Once again we shif and sum
+      Gyr_rawY=Wire.read()<<8|Wire.read();
+   
+      /*---X---*/
+      Gyro_raw_error_x = Gyro_raw_error_x + (Gyr_rawX/32.8); 
+      /*---Y---*/
+      Gyro_raw_error_y = Gyro_raw_error_y + (Gyr_rawY/32.8);
+      if(i==199)
+      {
+        Gyro_raw_error_x = Gyro_raw_error_x/200;
+        Gyro_raw_error_y = Gyro_raw_error_y/200;
+        gyro_error=1;
+      }
+    }
+  }  
+}//end of setup void
+
+
+
+
+
+
+void loop() {
+    timePrev = time;                        // the previous time is stored before the actual time read
+    time = millis();                        // actual time read
+    elapsedTime = (time - timePrev) / 1000; //divide by 1000 in order to obtain seconds
+
+    
+//////////////////////////////////////Gyro read/////////////////////////////////////
+
+    Wire.beginTransmission(0x68);            //begin, Send the slave adress (in this case 68) 
+    Wire.write(0x43);                        //First adress of the Gyro data
+    Wire.endTransmission(false);
+    Wire.requestFrom(0x68,4,true);           //We ask for just 4 registers
+        
+    Gyr_rawX=Wire.read()<<8|Wire.read();     //Once again we shif and sum
+    Gyr_rawY=Wire.read()<<8|Wire.read();
+    /*Now in order to obtain the gyro data in degrees/seconds we have to divide first
+    the raw value by 32.8 because that's the value that the datasheet gives us for a 1000dps range*/
+    /*---X---*/
+    Gyr_rawX = (Gyr_rawX/32.8) - Gyro_raw_error_x; 
+    /*---Y---*/
+    Gyr_rawY = (Gyr_rawY/32.8) - Gyro_raw_error_y;
+    
+    /*Now we integrate the raw value in degrees per seconds in order to obtain the angle
+    * If you multiply degrees/seconds by seconds you obtain degrees */
+    /*---X---*/
+    Gyro_angle_x = Gyro_angle_x  + Gyr_rawX*elapsedTime;
+    /*---X---*/
+    Gyro_angle_y = Gyro_angle_y  + Gyr_rawY*elapsedTime;
+
+    //Serial.print("GyroX raw: ");
+    //Serial.print(Gyr_rawX);
+    //Serial.print("   |   ");
+    Serial.print("GyroY raw: ");
+    Serial.println(Gyr_rawY);
+    //Serial.print("   |   ");
+    //Serial.print("GyroX angle: ");
+    //Serial.print(Gyro_angle_x);
+    //Serial.print("   |   ");
+    //Serial.print("GyroY angle: ");
+    //Serial.println(Gyro_angle_y);
+
+}
